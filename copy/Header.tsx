25f518d95923a478b7636db58f17707d903e31ea
Shin-sibainu <shincode0712@gmail.com>
2023-11-18 14:43:34 +0900

    header

--- copy/Header.tsx
+++ copy/Header.tsx
@@ -1,14 +1,9 @@
 "use client";
-
-import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  const { data: session } = useSession();
-  const user = session?.user;
-
   return (
     <header className="bg-slate-600 text-gray-100 shadow-lg">
       <nav className="flex items-center justify-between p-4">
@@ -23,32 +18,18 @@ const Header = () => {
             ホーム
           </Link>
           <Link
-            href={user ? "/profile" : "/login"}
+            href="/login"
             className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
           >
-            {user ? "プロフィール" : "ログイン"}
+            ログイン
           </Link>
-          {user ? (
-            <button
-              onClick={() =>
-                signOut({
-                  callbackUrl: "/login",
-                })
-              }
-              className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-            >
-              ログアウト
-            </button>
-          ) : (
-            ""
-          )}
 
           <Link href={`/profile`}>
             <Image
               width={50}
               height={50}
               alt="profile_icon"
-              src={user?.image || "/default_icon.png"}
+              src={"/default_icon.png"}
             />
           </Link>
         </div>
